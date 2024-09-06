cd481fbac83daf579caa5d7f02625669ac59e291
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-09-06 12:39:10 -0400

    Update plot_gaussian.py

--- PS-1/plot_gaussian.py
+++ PS-1/plot_gaussian.py
@@ -1,1 +1,17 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+o = 3 #standard deviation
+u = 0 #mean
+x = np.arange(-10,10,0.1) #gives inputs to the function
+
+gaussian = (o*np.sqrt(2*np.pi))**(-1)*np.exp(-0.5*((x-u)/o)**2)
+
+plt.plot(x,gaussian)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Normalized Gaussian Distribution")
+
+plt.savefig('guassian.png')
