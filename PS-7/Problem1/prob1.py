abaa5b063f770f9b3b69cae4e89c2cb4ba48d2d1
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-10-29 07:40:50 +0000

    created Lagrange Newton-Raphson function

--- PS-7/Problem1/prob1.py
+++ PS-7/Problem1/prob1.py
@@ -3,3 +3,16 @@ import matplotlib.pyplot as plt
 from scipy.optimize import brentq
 
 
+def Lagrange(r_0,R,M,m):
+    acc = 1e-6
+    r_delta = 0
+    r = r_0
+    while abs(r_delta) < acc:
+        f = (M/r**2) - (r*M/R**3) - (m/(R-r)**2)
+        f_prime = (-2*M/r**3) - (M/R**3) - (2*m/(R-r)**3)
+        r_delta = f/f_prime
+        r -= r_delta
+    return r
+
+
+
