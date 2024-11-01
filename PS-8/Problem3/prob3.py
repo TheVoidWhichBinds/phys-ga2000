63249c8377b2ffaab73cda5f83daef4a1797955b
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-11-01 00:41:56 +0000

    prob3 thru d)

--- PS-8/Problem3/prob3.py
+++ PS-8/Problem3/prob3.py
@@ -10,7 +10,7 @@ plt.title('Dow Jones Industrial Average')
 plt.xlabel('Day') #since 2006?
 plt.ylabel('Daily Closing Value ($)')
 plt.plot(dow, color = 'm')
-plt.savefig('Dow Jones from late 2006 to end of 2010')
+plt.savefig('DJ')
 
 #b )
 
@@ -20,16 +20,16 @@ c = np.fft.rfft(dow)
 
 ten_c = int(0.1*len(c))
 c_mod = np.zeros(len(c))
-c_mod[0:ten_c] = c[0:ten_c]
+c_mod[0:ten_c] = np.abs(c[0:ten_c])
 
 #d )
 
-#dow_ifft = np.fft.irfft(c)
-#plt.title('Dow Jones Industrial Average')
-#plt.xlabel('Day') #since 2006?
-#plt.ylabel('Daily Closing Value ($)')
-#plt.plot(dow, color = 'm', label = 'original data')
-#plt.plot(dow_ifft, color = 'r', label = 'Fourier-reconstructed data')
-#plt.savefig('Dow Jones from late 2006 to end of 2010')
-#plt.plot()
-#plt.plot( ,dowf, color = 'm')
+dow_ifft = np.fft.irfft(c_mod)
+plt.title('Dow Jones Industrial Average with Fourier Reconstruction')
+plt.xlabel('Day') #since 2006?
+plt.ylabel('Daily Closing Value ($)')
+
+plt.plot(dow, color = 'm', label = 'original data')
+plt.plot(dow_ifft, color = 'r', label = 'Fourier-reconstructed data')
+plt.legend()
+plt.savefig('DJR')
