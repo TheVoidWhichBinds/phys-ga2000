636c29633ad33b4228d8aa1b7853788ab33c90c9
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-11-01 19:09:57 +0000

    Finalized Problems 2 and 3

--- PS-8/Problem2/prob2.py
+++ PS-8/Problem2/prob2.py
@@ -7,28 +7,40 @@ trumpet = np.loadtxt('trumpet.txt')
 
 #a ) 
 def FFT(instrument, instrument_name):
- 
-    plt.figure(figsize=(18, 12))
-    plt.title(f'{instrument_name} Note', fontsize=20)
+    #Plots data.
+    plt.figure(figsize=(10, 6))
+    plt.title(f'{instrument_name} Note', fontsize=18)
     plt.xlabel('Time', fontsize=16)
     plt.ylabel('Amplitude', fontsize=16)
-    
-    plt.plot(np.arange(0, len(instrument)), instrument, color='b')
+    plt.plot(np.arange(0, len(instrument)), instrument, color='m')
     plt.savefig(f'{instrument_name}_Signal.png')
 
+    #X-axis of Fourier plots must be altered by the sampling rate in order to read frequency.
+    sampling_rate = 44100
+    frequencies = np.fft.rfftfreq(len(instrument[0:10000]), 1/sampling_rate) #Frequency spectrum.
+    c = np.fft.rfft(instrument[0:10000]) #Fast Fourier Transform to extract the coefficients.
 
-    c = np.fft.fft(instrument[0:10000])
-
-    plt.figure(figsize=(18, 12))
-    plt.title(f'{instrument_name} Note Fourier Coefficients', fontsize=20)
+    #Plotting Fourier coefficients on a frequency vs. magnitude graph.
+    plt.figure(figsize=(10, 6))
+    plt.title(f'{instrument_name} Note Fourier Coefficients', fontsize=18)
     plt.xlabel('Frequency', fontsize=16)
+    plt.xlim(0,5000)
     plt.ylabel('Magnitude', fontsize=16)
-    plt.plot(abs(c))
-    plt.savefig(f'{instrument_name}_FFT.png')  
+    plt.plot(frequencies, abs(c))
+    plt.savefig(f'{instrument_name}_FFT.png') 
+
+
+    #b ) 
+    #Finds the fundamental frequency of the instrument.
+    threshold = 0.4 * 1e7
+    for i, magnitude in enumerate(np.abs(c)): #Looks over all coefficients.
+        if magnitude > threshold: #We only care about coefficients of significant (arbitrary) contribution.
+            print(f'The note being played on the {instrument_name} has a fundamental frequency of', frequencies[i],'Hz')
+            break #Only reads off the fundamental frequency, which is all that is needed to find the note.
+    
 
 FFT(piano, 'Piano')
 FFT(trumpet, 'Trumpet')
 
 
 
-#b ) 
--- PS-8/Problem3/prob3.py
+++ PS-8/Problem3/prob3.py
@@ -5,31 +5,42 @@ import matplotlib.pyplot as plt
 dow = np.loadtxt('dow.txt')
 
 #a )
-
+#Plots Dow Jones data.
 plt.title('Dow Jones Industrial Average')
 plt.xlabel('Day') #since 2006?
 plt.ylabel('Daily Closing Value ($)')
-plt.plot(dow, color = 'm')
+plt.plot(dow, color = 'g')
 plt.savefig('DJ')
 
 #b )
-
+#Extracting Fourier coefficients using Fast Fourier Transform, only the real component so we don't have redundancy.
 c = np.fft.rfft(dow)
 
 #c )
 
-ten_c = int(0.1*len(c))
-c_mod = np.zeros(len(c))
-c_mod[0:ten_c] = np.abs(c[0:ten_c])
+def reconstructed(percentage):
+    
+    nonzero = int(percentage*len(c)) #Calculates the number of indices of c we want to keep based on the input percentage.
+    c_mod = np.zeros(len(c), dtype=complex) #Initializes new coefficient array.
+    c_mod[0:nonzero] = np.abs(c[0:nonzero]) #Fills the first {percentage}% of the new coefficient array with the c.
+    dow_ifft = np.fft.irfft(c_mod) #Reconstructs the data based on the new coeffficent array.
+
+    #Plots the reconstructed data over the original data. Setting percentage to 100% verifies that the original data is recovered.
+    plt.figure()
+    plt.title('Dow Jones Industrial Average with Fourier Reconstruction')
+    plt.xlabel('Day') #since 2006?
+    plt.ylabel('Daily Closing Value ($)')
+    plt.plot(dow, color = 'g', label = 'original data')
+    plt.plot(dow_ifft, color='m', label=f'First {percentage*100:.1f}% of Fourier coefficients')
+    plt.legend()
+    plt.savefig(f'DJR_{int(percentage * 100)}%.png')  # Add file extension
+    plt.close()  # Close the plot
+
 
 #d )
+reconstructed(0.1)
 
-dow_ifft = np.fft.irfft(c_mod)
-plt.title('Dow Jones Industrial Average with Fourier Reconstruction')
-plt.xlabel('Day') #since 2006?
-plt.ylabel('Daily Closing Value ($)')
+#e )
+reconstructed(0.02)
 
-plt.plot(dow, color = 'm', label = 'original data')
-plt.plot(dow_ifft, color = 'r', label = 'Fourier-reconstructed data')
-plt.legend()
-plt.savefig('DJR')
+
