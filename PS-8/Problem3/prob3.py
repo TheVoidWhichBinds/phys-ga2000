1c785724cc7d1a3bce2dcf625b7a2ff5a9186087
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-11-01 00:32:45 +0000

    Problem 3

--- PS-8/Problem3/prob3.py
+++ PS-8/Problem3/prob3.py
@@ -2,26 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#import dow.txt
-pts = len(dow)
+dow = np.loadtxt('dow.txt')
+
 #a )
 
 plt.title('Dow Jones Industrial Average')
-plt.xlabel('')
+plt.xlabel('Day') #since 2006?
 plt.ylabel('Daily Closing Value ($)')
-plt.plot()
+plt.plot(dow, color = 'm')
+plt.savefig('Dow Jones from late 2006 to end of 2010')
 
 #b )
 
-c = np.fft.rfft()
+c = np.fft.rfft(dow)
 
 #c )
 
-c[10:100,:] = 0
+ten_c = int(0.1*len(c))
+c_mod = np.zeros(len(c))
+c_mod[0:ten_c] = c[0:ten_c]
 
 #d )
 
-dowf = np.fft.irfft(c)
-#copy part a plot details
-plt.plot()
-plt.plot( ,dowf, color = 'm')
+#dow_ifft = np.fft.irfft(c)
+#plt.title('Dow Jones Industrial Average')
+#plt.xlabel('Day') #since 2006?
+#plt.ylabel('Daily Closing Value ($)')
+#plt.plot(dow, color = 'm', label = 'original data')
+#plt.plot(dow_ifft, color = 'r', label = 'Fourier-reconstructed data')
+#plt.savefig('Dow Jones from late 2006 to end of 2010')
+#plt.plot()
+#plt.plot( ,dowf, color = 'm')
