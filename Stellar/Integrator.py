next step: need to fix overflow on dT_dm

Stellar/Integrator.py
@@ -89,13 +89,21 @@ def ODESolver(initial_conditions, num_iter, extra_params, inwards, verbose=False
     output = np.zeros((num_iter, 6))
     output[0,:] = current
     
+    inwards_deriv = None
+    outwards_deriv = None
     for i in range(1, num_iter):
         #RK4 receives a specific differential equation corresponding to each variable of interest. 
         #RK4 outputs a 6x1 array with elements of: mass, radius, pressure, luminosity, 
         #temperature, and density. Only the element corresponding to the differential equation (derivatives[n])
         #input into RK4 has the correct updated value
+        
+        if i == num_iter//2:
+            if inwards:
+                inwards_deriv = derivative_calc(current, extra_params)
+            else:
+                outwards_deriv = derivative_calc(current, extra_params)
+            
         update  = RK4(derivative_calc, current, step_size, extra_params, inwards)
-        #
         #if(verbose):
             #print(i, current, update)
         #if(np.any(np.isnan(update))):
@@ -104,14 +112,11 @@ def ODESolver(initial_conditions, num_iter, extra_params, inwards, verbose=False
         #cur_state = cur_state + delta #                naming this "delta" is misleading - RK4 outputs the new state, not the delta between states - renamed instances of "delta" to "update"
         current = update
         current[DENSITY_UNIT_INDEX] = equation_of_state(current[PRESSURE_UNIT_INDEX], current[TEMP_UNIT_INDEX], extra_params)
-        
         # if(np.any(np.less(current,0)) or (np.any(np.isnan(current)))):
         #     break
         output[i,:] = current
-    
-    #print(np.shape(output[:,MASS_UNIT_INDEX]))
-    #print(np.shape(output))
-    return output
+    #print(output)
+    return output, inwards_deriv, outwards_deriv
 
 
 if __name__ == "__main__": #                            Purpose of having this at the end?

Stellar/Minimizer.py
@@ -71,14 +71,16 @@ def smooth_merge(bound_guess, num_iter, extra_params, step_size):
     assert(T_core_guess >= 0)
     assert(L_outer_guess >= 0)
 
-    outwards = Integrator.ODESolver(gen_core_conditions(P_core_guess, T_core_guess, step_size, extra_params), num_iter, extra_params, False)
-    inwards =  Integrator.ODESolver(gen_outer_conditions(L_outer_guess), num_iter, extra_params, True)
-    print(inwards)
-    diff = np.sum(outwards[num_iter//2,:] - inwards[num_iter//2,:])
+    _,_,outwards_deriv = Integrator.ODESolver(gen_core_conditions(P_core_guess, T_core_guess, step_size, extra_params), num_iter, extra_params, False)
+    _,inwards_deriv,_ = Integrator.ODESolver(gen_outer_conditions(L_outer_guess), num_iter, extra_params, True)
+
+    print(outwards_deriv)
+    diff = np.sum(np.abs(outwards_deriv - inwards_deriv))
+
     diff_weight = 1
     
-    core_conditions_solved = outwards[0,[0,1,4]]
-    outer_conditions_solved = inwards[0,:]
+    #core_conditions_solved = outwards[0,[0,1,4]]
+    #outer_conditions_solved = inwards[0,:]
     #boundary = np.sum(np.abs(Utilities.global_tolerance*np.array([1,1,1]) - core_conditions_solved))  +  np.sum(np.abs(np.array([1,1,0,0,1,0]) - outer_conditions_solved))
     boundary_weight = 0
     return diff_weight * diff**2 + boundary_weight#boundary